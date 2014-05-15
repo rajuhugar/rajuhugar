442d956e76faa3f9c54ef049cb7e75ee39b5e092
unknown <RaHugar@RAHUGAR01.corp.amdocs.com>
2014-05-15 13:28:33 +0100

    adding new file

---
+++ src/test/java/com/alphacsp/training/AppTest.java
@@ -0,0 +1,14 @@
+package com.alphacsp.training;
+
+import org.junit.Test;
+
+/**
+ * test for simple App.
+ */
+public class AppTest{
+
+    @Test
+    public void testApp() {
+        new App().greeting();
+    }
+}
